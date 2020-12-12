46421c783f6d936a026bbfc5587191db55e83938
dzhelez2903 <dzhelez@yahoo.com>
2020-12-12 18:31:51 +0200

    task 1 done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-function myFunction(p) {
-  return p * p;
-}
+//Task1
 button = document.getElementById("task_1_button");
-button.onclick = function() {
-   let x = document.getElementById("task_1").value;
-   document.getElementById("task_1_answer").innerText = myFunction(x); 
+button.onclick = function () {
+  let x = document.getElementById("task_1").value;
+  if (isNaN(x)) {
+    document.getElementById("task_1_answer").textContent = "Циферки введи!";
+  } else if (x >= 0 && x < 3) {
+    document.getElementById("task_1_answer").textContent = "младенец";
+  } else if (x >= 3 && x <= 11) {
+    document.getElementById("task_1_answer").textContent = "отрок";
+  } else if (x >= 12 && x <= 18) {
+    document.getElementById("task_1_answer").textContent = "юноша";
+  } else if (x >= 19 && x <= 59) {
+    document.getElementById("task_1_answer").textContent = "муж";
+  } else if (x > 60) {
+    document.getElementById("task_1_answer").textContent = "старец";
+  }
 };
